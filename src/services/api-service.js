c4ad77c454ebb1f3f82d02d40d1e0e21824046a1
Jae Hward <howardjake9512@gmail.com>
2021-05-01 16:00:44 -0400

    deployment url change

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,10 +1,10 @@
 //Daniels Notice Lab
 
 // TODO: eventually change this to heroku
-const BASE_URL = "http://localhost:3000/assignments";
-const MASTERS_URL = "http://localhost:3000/assignment_masters";
-const STUDENT_URL = "http://localhost:3000/students";
-const DASHBOARD_URL = "http://localhost:3000/dashboard"
+const BASE_URL = "https://gradebook-backend.herokuapp.com/assignments";
+const MASTERS_URL = "https://gradebook-backend.herokuapp.com/assignment_masters";
+const STUDENT_URL = "https://gradebook-backend.herokuapp.com/students";
+const DASHBOARD_URL = "https://gradebook-backend.herokuapp.com/dashboard"
 
 function fetchAssignments() {
 	return fetch(BASE_URL).then((res) => res.json());
