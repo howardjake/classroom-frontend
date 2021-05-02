20fc71e412a8024527badaac43afccad93c85467
Jae Hward <howardjake9512@gmail.com>
2021-05-02 12:37:57 -0400

    new hero title

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,13 @@
 function Welcome(props) {
 	return <div id="welcome"> 
 	<video className="video_bg" src="production ID_5192104.mp4" muted autoPlay loop></video>
-	<div>
-		<h1>Login</h1>
+	<div className="welcome_card">
+		<h3>Welcome to your new</h3>
+		<div className="hero_name">
+		<h1 >Gradebook</h1>
+		</div>
+		
+
 	</div>
 	
 	
