55bf3dbd8dd08a47bdddc107a604ecaf87a325ab
Jae Hward <howardjake9512@gmail.com>
2021-05-02 14:25:15 -0400

    remove warnings

--- src/Pages/Student.js
+++ src/Pages/Student.js
@@ -4,9 +4,9 @@ import StudentData from "../components/StudentData";
 function Student({ students, assignments, handleUpdate }) {
 	let { id } = useParams();
 
-	const currentStudent = students.filter((student) => student.id == id)[0];
+	const currentStudent = students.filter((student) => student.id === id)[0];
 	const studentAssignments = assignments.filter(
-		(assignment) => assignment.student_id == id
+		(assignment) => assignment.student_id === id
 	);
 
 	return (
